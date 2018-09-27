1cd0917b41b9be00378fcbf98a935cd2a195be9c
Fuzoka <unbeatablesites09@gmail.com>
2018-09-27 14:41:37 -0400

    Homework Done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <header className="footer">
+        <p>Clicky Game 2018</p>
+    </header>
+);
+
+export default Footer;
